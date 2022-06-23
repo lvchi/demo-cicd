b318baf8718e465a95b46a7a573a076221e5813d
Chi <chi.lam@int.easycredit.vn>
2022-06-23 21:31:16 +0700

    update1

--- codedeploy/start_server.sh
+++ codedeploy/start_server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # source /home/ec2-user/web/.env
-$(aws ecr get-login --region $AWS_DEFAULT_REGION --no-include-email)aws ecr get-login-password | docker login --username AWS --password-stdin 
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 434839081440.dkr.ecr.us-east-1.amazonaws.com
 cd /home/ec2-user/web/ && /usr/local/bin/docker-compose -f docker-compose.yml up -d
